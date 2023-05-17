6509c22e7f4d3923a0910541e3bbfd835c402377
Taleb <133849860+1taleb1@users.noreply.github.com>
2023-05-17 16:14:46 +0600

    Create task.py

---
+++ task.py
@@ -0,0 +1,19 @@
+import db
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, cast, Date, func
+from datetime import datetime
+import collections
+import json
+import urllib
+
+
+def flatten(d, parent_key='', sep='_'):
+    items = []
+    for k, v in d.items():
+        new_key = parent_key + sep + k if parent_key else k
+        if isinstance(v, collections.abc.MutableMapping):
+            items.extend(flatten(v, new_key, sep=sep).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
